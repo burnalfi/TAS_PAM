3c0bd50a08a31a2227273d30d582fb317d04a910
Adi Prasetyo <goendoell49@gmail.com>
2020-04-04 19:43:11 +0700

    acurate GMap activity   - copy-cat PermissionUtils from sample

--- app/src/main/java/com/example/tas_pam/MainActivity.java
+++ app/src/main/java/com/example/tas_pam/MainActivity.java
@@ -1,5 +1,8 @@
 package com.example.tas_pam;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Menu;
@@ -8,6 +11,8 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.navigation.NavigationView;
 
+import androidx.annotation.NonNull;
+import androidx.core.content.ContextCompat;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -16,9 +21,10 @@ import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+  private static final int LOCATION_PERMISSION_REQUEST_CODE = 1;
   private AppBarConfiguration mAppBarConfiguration;
+  private boolean mPermissionDenied;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -27,13 +33,7 @@ public class MainActivity extends AppCompatActivity {
     Toolbar toolbar = findViewById(R.id.toolbar);
     setSupportActionBar(toolbar);
     FloatingActionButton fab = findViewById(R.id.fab);
-    fab.setOnClickListener(new View.OnClickListener() {
-      @Override
-      public void onClick(View view) {
-        Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show();
-      }
-    });
+    fab.setOnClickListener(this);
     DrawerLayout drawer = findViewById(R.id.drawer_layout);
     NavigationView navigationView = findViewById(R.id.nav_view);
     // Passing each menu ID as a set of Ids because each
@@ -45,8 +45,37 @@ public class MainActivity extends AppCompatActivity {
     NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
     NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
     NavigationUI.setupWithNavController(navigationView, navController);
+
+    enableMyLocation();
+  }
+  private void enableMyLocation() {
+    if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+            == PackageManager.PERMISSION_GRANTED) {
+
+    } else {
+      // Permission to access the location is missing. Show rationale and request permission
+      PermissionUtils.requestPermission(this, LOCATION_PERMISSION_REQUEST_CODE,
+              Manifest.permission.ACCESS_FINE_LOCATION, true);
+    }
   }
 
+  @Override
+  public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+    if (requestCode != LOCATION_PERMISSION_REQUEST_CODE) {
+      return;
+    }
+
+    if (PermissionUtils.isPermissionGranted(permissions, grantResults, Manifest.permission.ACCESS_FINE_LOCATION)) {
+      // Enable the my location layer if the permission has been granted.
+      enableMyLocation();
+    } else {
+      // Permission was denied. Display an error message
+      // [START_EXCLUDE]
+      // Display the missing permission error dialog when the fragments resume.
+      mPermissionDenied = true;
+      // [END_EXCLUDE]
+    }
+  }
   @Override
   public boolean onCreateOptionsMenu(Menu menu) {
     // Inflate the menu; this adds items to the action bar if it is present.
@@ -59,5 +88,11 @@ public class MainActivity extends AppCompatActivity {
     NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
     return NavigationUI.navigateUp(navController, mAppBarConfiguration)
             || super.onSupportNavigateUp();
+  }
+
+  @Override
+  public void onClick(View v) {
+    Intent intent= new Intent(MainActivity.this, MapsOfCafe.class);
+    startActivity(intent);
   }
 }
--- app/src/main/java/com/example/tas_pam/MapsOfCafe.java
+++ app/src/main/java/com/example/tas_pam/MapsOfCafe.java
@@ -1,8 +1,14 @@
 package com.example.tas_pam;
 
+import androidx.annotation.NonNull;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.FragmentActivity;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -11,7 +17,8 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapsOfCafe extends FragmentActivity implements OnMapReadyCallback {
+public class MapsOfCafe extends FragmentActivity implements OnMapReadyCallback, GoogleMap.OnMyLocationButtonClickListener, GoogleMap.OnMyLocationClickListener {
+
 
   private GoogleMap mMap;
 
@@ -39,10 +46,26 @@ public class MapsOfCafe extends FragmentActivity implements OnMapReadyCallback {
   public void onMapReady(GoogleMap googleMap) {
     mMap = googleMap;
 
+    mMap.setOnMyLocationButtonClickListener(this);
+    mMap.setOnMyLocationClickListener(this);
     // Add a marker in a. live b. kanfak fti
+    if (mMap != null) {
+      mMap.setMyLocationEnabled(true);
+    }
+  }
 
-    LatLng sydney = new LatLng(-34, 151);
-    mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-    mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+
+  @Override
+  public void onMyLocationClick(@NonNull Location location) {
+//    implication??
+    Toast.makeText(this, "Current location:\n" + location, Toast.LENGTH_LONG).show();
+  }
+
+  @Override
+  public boolean onMyLocationButtonClick() {
+    Toast.makeText(this, R.string.open_map_activity, Toast.LENGTH_SHORT).show();
+    // Return false so that we don't consume the event and the default behavior still occurs
+    // (the camera animates to the user's current position).
+    return false;
   }
 }
